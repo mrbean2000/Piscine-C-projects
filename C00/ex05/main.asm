643f384ae3b90aafba154a9b24c850d7c7ead2bc
Koh Chin Woon <chinwoon_koh@hotmail.com>
2025-04-15 22:18:47 +0800

    Completed ft_print_comb exercise

---
+++ C00/ex05/main.asm
@@ -0,0 +1,15 @@
+; This program will call the function that will print out all unique conmibinations of 3 different digits
+; For reference to the system call table, follow the following URL:
+;	https://www.chromium.org/chromium-os/developer-library/reference/linux-constants/syscalls/
+
+%include "ft_print_comb.asm"
+
+global _start
+_start:
+	mov	rbp, rsp
+
+	call	ft_print_comb
+
+	mov	rax, 0x3c
+	mov	rdi, 0;
+	syscall
