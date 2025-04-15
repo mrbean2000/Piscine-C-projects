c9af7aec228be789e9b24929a30daf9bc25cb1a7
Koh Chin Woon <chinwoon_koh@hotmail.com>
2025-04-15 18:47:48 +0800

    Completed ft_is_negative

---
+++ C00/ex04/ft_is_negative.asm
@@ -0,0 +1,51 @@
+; This file contains a function that test to see if the number that was passed is negative or positive
+
+extern	INT_SIZE
+extern	CHAR_SIZE
+
+STDOUT		equ	1
+WRITE		equ	1
+SIZE		equ	1
+
+SIGN_FLAG_POS	equ	15
+
+; INT_SIZE + CHAR_SIZE + n * 8 byte address from push in this function
+; plus 8 bytes for return address
+TOTAL_OFFSET	equ	49
+
+global ft_is_negative
+ft_is_negative:
+	push	rax
+	push	rdi
+	push	rsi
+	push	rdx
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 1;
+
+	cmp	dword [rbp + TOTAL_OFFSET], 0
+	lahf
+	shr	rax, SIGN_FLAG_POS
+
+	test	rax, 0x1
+	jnz	.print_neg
+	mov	byte [rbp - 1], 'P'
+	jmp	.endif
+.print_neg:
+	mov	byte [rbp - 1], 'N'
+
+.endif:
+	mov	rax, WRITE
+	mov	rdi, STDOUT
+	lea	rsi, [rbp - 1]
+	mov	rdx, COUNT
+	syscall
+
+	add	rsp, 1
+	pop	rbp
+	pop	rdx
+	pop	rsi
+	pop	rdi
+	pop	rax
+	ret
+	
---
+++ C00/ex04/main.asm
@@ -0,0 +1,26 @@
+; This file will call a function that determine of the value that is passed into the function is negative
+; or not. The value that is tested will be printed out in the main function.
+; The system call table will be at the following URL:
+;	https://www.chromium.org/chromium-os/developer-library/reference/linux-constants/syscalls
+
+%include "ft_is_negative.asm"
+
+INT_SIZE	equ	4
+WRITE		equ	1
+COUNT		equ	1
+STDOUT		equ	1
+CHAR_SIZE	equ	1
+
+global _start
+_start:
+	mov	rbp, rsp
+	sub	rsp, INT_SIZE + CHAR_SIZE
+
+	; edit the integer value for the test
+	mov	dword [rbp - INT_SIZE], -20
+
+	call	ft_is_negative
+
+	mov	rax, 0x3c
+	mov	rdi, 0
+	syscall
